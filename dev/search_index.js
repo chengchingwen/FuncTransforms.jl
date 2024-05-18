8cb361afe796b0d88e44c3c4ab43d4f682ea3ce6
Documenter.jl <documenter@juliadocs.github.io>
2024-05-18 01:38:10 +0000

    build based on e8b709f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FuncTransforms","category":"page"},{"location":"#FuncTransforms","page":"Home","title":"FuncTransforms","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FuncTransforms.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FuncTransforms]","category":"page"}]
+}
